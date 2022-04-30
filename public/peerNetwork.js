1794c5bf8feeb62ad478f657f5faeed9c4d17495
joe <moonjose@gmail.com>
2022-04-30 07:12:42 -0500

    transfer commit

--- public/peerNetwork.js
+++ public/peerNetwork.js
@@ -1,26 +1,75 @@
-const socket = io('/');
+const socket = io("/");
 let networkPeers = [];
+let selfID = ""; // ID specific to this peer
 
-// PeerJS: API that allows the server to generate a peerId 
+// PeerJS: API that allows the server to generate a peerId
 const peer = new Peer(undefined, {
-    host: '/', 
-    port: '3001'
+    host: "/",
+    port: "3001",
 });
+
+const file = document.getElementById("file");
+
 
 // Open a connection to the network for a newly created peer
-peer.on('open', peerId => {
-    socket.emit('join-network', NETWORK_ID, peerId);
+peer.on("open", (peerId) => {
+    selfID = peerId;
+    socket.emit("join-network", NETWORK_ID, peerId);
 });
 
-socket.on('peer-connected', (peerList) => {
+socket.on("peer-connected", (peerList) => {
     console.log("Peer Connected.");
 
     networkPeers = peerList.splice(0);
     console.log(networkPeers);
+
+    printPeerDisplay(networkPeers);
 });
 
-socket.on('peer-disconnected', (peerId) => {
+socket.on("peer-disconnected", (peerId) => {
     console.log("Peer Disconnected.");
     networkPeers.splice(networkPeers.indexOf(peerId), 1);
     console.log("peers on network: ", networkPeers);
+    printPeerDisplay(networkPeers);
 });
+
+const printPeerDisplay = (networkPeers) => {
+    refreshPeerDisplay();
+
+    const peerDisplay = document.getElementById("peerList");
+
+    networkPeers.forEach((peer, index) => {
+        const newRow = document.createElement("div");
+        if (peer == selfID) {
+            newRow.className = "row self";
+        } else {
+            newRow.className = "row";
+        }
+        newRow.innerHTML = `<div class="col-2 text-center">${index}</div><div class="col text-center">${peer}</div>`;
+        peerDisplay.append(newRow);
+    });
+};
+
+const refreshPeerDisplay = () => {
+    const peerDisplay = document.getElementById("peerList");
+    peerDisplay.innerHTML = ``;
+};
+
+
+const uploadFile = () => {
+    console.log("UPLOADFILE => ", file.files); 
+    if (file.files.length > 0) {
+        const fileInfo = file.files[0];
+        const filemeta = {
+            lastModified: fileInfo.lastModified,
+            lastModifiedDate: fileInfo.lastModifiedDate,
+            name: fileInfo.name,
+            size: fileInfo.size,
+            type: fileInfo.type,
+            webkitRelativePath: fileInfo.webkitRelativePath
+        };
+        socket.emit("registerFile", selfID, filemeta);
+    }
+}
+
+document.getElementById("upload").addEventListener("click", uploadFile);
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const { v4: uuidV4 } = require('uuid');
 
-let peerList = [];  // array of all peerIds connected to the network
+let peerList = [];      // array of all peerIds connected to the network
+let fileLibrary = [];   // array of available files on the network
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -32,7 +33,8 @@ io.on('connection', (socket) => {
         // Update peerList with new peerId
         if (!peerList.includes(peerId)) {
             peerList.push(peerId);
-            console.log(peerList);
+            console.log("peerList => ", peerList);
+            console.log(fileLibrary);
         }
 
         // Announce existence to all peers on the network with same networkId
@@ -41,7 +43,17 @@ io.on('connection', (socket) => {
         // STEP 4: disconnect from the network when the peer leaves
         socket.on('disconnect', () => {
             peerList.splice(peerList.indexOf(peerId), 1);
+            clearLibrary(peerId);
             socket.to(networkId).emit('peer-disconnected', peerId);
+        });
+
+        socket.on('registerFile', (peerId, file) => {
+            const fileData = {
+                key: peerId,
+                value: file
+            };
+            fileLibrary.push(fileData);
+            console.log("fileLibrary => ", fileLibrary); 
         });
     });
 });
@@ -49,7 +61,18 @@ io.on('connection', (socket) => {
 const updatePeers = (socket, networkId) => {
     socket.in(networkId).emit('peer-connected', peerList);
     socket.emit('peer-connected', peerList);
-}
+};
+
+// Removes indexing of peerId's files from the fileLibrary
+const clearLibrary = (peerId) => {
+    // console.log("clearLibrary => ", fileLibrary);
+    fileLibrary.slice().reverse().forEach((fileInfo, fileIndex, fileObject) => {
+        if (fileInfo.key == peerId) {
+            fileLibrary.splice(fileObject.length - 1 - fileIndex, 1);
+        }
+    });
+    // console.log("AFTER CLEAR => ", fileLibrary);
+};
 
 //const port = process.env.PORT || 3000;
 const port = 3000;
